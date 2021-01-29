cf0fb007dbc3c39dcf297dbb3aaa2ce1c98db325
Tristan-Fullmer <ful15007@byui.edu>
2021-01-29 12:16:22 -0600

    rename method
    Also added a test functionality

--- dl__reader/models/models.py
+++ dl__reader/models/models.py
@@ -6,5 +6,6 @@ from odoo import models, fields, api
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    def test(self):
+    def action_scan_license(self):
+        self.name = "Silly Corp"
         return 0
