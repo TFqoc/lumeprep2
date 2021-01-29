e7c8a7a34be3d2e6baec7d4ba65e56ca9125d4eb
Tristan-Fullmer <ful15007@byui.edu>
2021-01-29 12:01:03 -0600

    add test method to res.partner

--- dl__reader/models/models.py
+++ dl__reader/models/models.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+ from odoo import models, fields, api
 
 
-# class dl__reader(models.Model):
-#     _name = 'dl__reader.dl__reader'
-#     _description = 'dl__reader.dl__reader'
+class Partner(models.Model):
+    _inherit = 'res.partner'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    def test(self):
+        return 0
