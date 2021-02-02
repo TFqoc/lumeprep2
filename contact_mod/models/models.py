bac3d5ca0bfdde29f01fd98ccd525eda341b4724
Tristan-Fullmer <ful15007@byui.edu>
2021-02-02 09:47:43 -0600

    Add fields

--- contact_mod/models/models.py
+++ contact_mod/models/models.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+import datetime
 
 
-# class contact_mod(models.Model):
-#     _name = 'contact_mod.contact_mod'
-#     _description = 'contact_mod.contact_mod'
+class Partner(models.Model):
+    _inherit = 'res.partner'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    medical_id = fields.Char()
+    medical_expiration = fields.Date()
+    date_of_birth = fields.Date()
+    is_over_21 = fields.Boolean(compute='_compute_21')
+    drivers_licence_number = fields.Char()
+    drivers_licence_expiration = fields.Date()
+
+    @api.depends('is_over_21')
+    def _compute_21(self):
+        for record in self:
+            record.is_over_21 = datetime.date.today().year - self.date_of_birth.year >= 21
