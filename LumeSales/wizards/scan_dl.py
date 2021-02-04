e9dfc44e5d290092e44e61a0b0ecc42f0b97c316
Tristan-Fullmer <ful15007@byui.edu>
2021-02-04 12:55:22 -0600

    Add description

--- LumeSales/wizards/scan_dl.py
+++ LumeSales/wizards/scan_dl.py
@@ -2,6 +2,7 @@ from odoo import models, fields
 
 class ScanDL(models.TransientModel):
     _name = "scan_dl"
+    _description = "scan_dl"
 
     image = fields.Binary("Image", help="Select image here")
     #<field name="image" widget='image' />
