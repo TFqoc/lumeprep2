b0e993e47aed0d25f1000ea177c5b547354fd361
Tristan-Fullmer <ful15007@byui.edu>
2021-02-08 14:00:36 -0600

    give basic permissions

--- LumeSales/__manifest__.py
+++ LumeSales/__manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'wizards/scan_dl.xml',
         'views/views.xml',
         'views/templates.xml',
