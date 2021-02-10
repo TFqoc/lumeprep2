6a81fbc77715b86ef7d6b5f1f2c887d8d41b0720
Tristan-Fullmer <ful15007@byui.edu>
2021-02-10 10:52:04 -0600

    Update demo.js

--- LumeSales/js/demo.js
+++ LumeSales/js/demo.js
@@ -6,4 +6,6 @@ odoo.define('LumeSales.Demo', ['web.kanban_record_quick_create'], function(requi
     Quick.include({
         events:{'click .o_kanban_cancel': '_onCancelClicked',},
     });
+
+    return Quick;
 });
