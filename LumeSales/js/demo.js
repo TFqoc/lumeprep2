94069044133954ce328ef1cce2e13bbf0eb9d550
Tristan-Fullmer <ful15007@byui.edu>
2021-02-10 13:12:06 -0600

    Update demo.js

--- LumeSales/js/demo.js
+++ LumeSales/js/demo.js
@@ -6,6 +6,8 @@ odoo.define('LumeSales.Demo', ['web.kanban_record_quick_create'], function(requi
     var Quick = require('web.kanban_record_quick_create');
     Quick.include({
         _add: function (options) {
+            console.log("Arguments ", arguments)
+            console.log("Options", options)
             this._super.apply(this, arguments);
             return this._rpc({
                 model: 'project.task',
