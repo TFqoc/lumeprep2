19eb5e12172699739c1c3cd2b0353782e037efb3
Tristan-Fullmer <ful15007@byui.edu>
2021-02-10 14:55:57 -0600

    Update unread_messages.js

--- LumeSales/js/unread_messages.js
+++ LumeSales/js/unread_messages.js
@@ -7,7 +7,7 @@ odoo.define('LumeSales.Unread_Messages', ['web.rpc'], function(require){
     function update_icon(){
         var model = 'project.task';
         // Use an empty array to search for all the records
-        var domain = [['message_unread', '=', true]];
+        var domain = [['message_unread_counter', '>', 0]];
         // Use an empty array to read all the fields of the records
         var fields = [];
         rpc.query({
@@ -16,6 +16,8 @@ odoo.define('LumeSales.Unread_Messages', ['web.rpc'], function(require){
             args: [domain, fields],
         }).then(function (data) {
             console.log(data);
+            // Loop through records
+            // Update associated card
         });
     }
     async function loop(){
