4efc7c05ad2219284683409b31747bb6f48f67f5
Tristan-Fullmer <ful15007@byui.edu>
2021-02-11 08:55:35 -0600

    Update live_timer.js

--- LumeSales/js/live_timer.js
+++ LumeSales/js/live_timer.js
@@ -1,21 +1,22 @@
 //alert("Unread Messages has been loaded");
-console.log("Unread Messages has been loaded");
+console.log("Live Timer has been loaded");
 odoo.define('LumeSales.live_timer', ['web.rpc'], function(require){
     "use strict";
 
-    var rpc = require('web.rpc')
+    var rpc = require('web.rpc');
+    var model = 'project.task';
     function update_timer(data){
         //Loop through records
         // update associated cards
     }
     function get_tasks(){
-        var model = 'project.task';
+        
         // Use an empty array to search for all the records
         var domain = [['is_timer_running', '=', true]];
         // Use an empty array to read all the fields of the records
         var fields = ['timer_counter'];
         task_data = rpc.query({
-            model: model,
+            model: this.model,
             method: 'search_read',
             args: [domain, fields],
         }).then(function (data) {
