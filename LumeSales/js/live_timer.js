816aa385857d510610141d5940698a1820195b33
Tristan-Fullmer <ful15007@byui.edu>
2021-02-11 08:39:31 -0600

    live timer

---
+++ LumeSales/js/live_timer.js
@@ -0,0 +1,39 @@
+//alert("Unread Messages has been loaded");
+console.log("Unread Messages has been loaded");
+odoo.define('LumeSales.live_timer', ['web.rpc'], function(require){
+    "use strict";
+
+    var rpc = require('web.rpc')
+    function update_timer(data){
+        //Loop through records
+        // update associated cards
+    }
+    function get_tasks(){
+        var model = 'project.task';
+        // Use an empty array to search for all the records
+        var domain = [['is_timer_running', '=', true]];
+        // Use an empty array to read all the fields of the records
+        var fields = ['timer_counter'];
+        task_data = rpc.query({
+            model: model,
+            method: 'search_read',
+            args: [domain, fields],
+        }).then(function (data) {
+            console.log(data);
+            // Loop through records
+            // Update associated card
+            return data;
+        });
+    }
+    async function loop(){
+        data = get_tasks();
+        while (true){
+            update_timer(data);
+            await new Promise(r => setTimeout(r, 1000));
+        }
+    }
+
+    loop();
+
+    return model;
+});
--- LumeSales/js/unread_messages.js
+++ LumeSales/js/unread_messages.js
@@ -4,24 +4,25 @@ odoo.define('LumeSales.Unread_Messages', ['web.rpc'], function(require){
     "use strict";
 
     var rpc = require('web.rpc')
+    var model = 'project.task';
     function update_icon(){
-        var model = 'project.task';
+        
         // Use an empty array to search for all the records
         var domain = [['id', '>', 10]];
         // Use an empty array to read all the fields of the records
         var fields = [];
         rpc.query({
-            model: model,
+            model: this.model,
             method: 'search_read',
             args: [domain, fields],
         }).then(function (data) {
-            console.log(data);
+            //console.log(data);
             // Loop through records
             // Update associated card
         });
     }
     async function loop(){
-        while (true){
+        while (false){
             update_icon();
             await new Promise(r => setTimeout(r, 10000));
         }
