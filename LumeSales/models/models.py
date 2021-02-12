77eec50109d55c037999e1cafee6de28ffb30345
Tristan-Fullmer <ful15007@byui.edu>
2021-02-12 10:07:58 -0600

    Update models.py

--- LumeSales/models/models.py
+++ LumeSales/models/models.py
@@ -53,7 +53,7 @@ class tasks(models.Model):
     def _onchange_partner(self):
         #self.message = "Dear %s" % (self.partner_id.name or "")
         return {
-            'warning': {'title': "Warning", 'message': "What is this?", 'type': 'notification'},
+            'warning': {'title': "Warning", 'message': "What is this?", 'type': 'warning'},
             }
 
     # @api.model
