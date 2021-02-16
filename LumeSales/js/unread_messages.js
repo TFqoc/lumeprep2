c8935211246c925e77e238679d84fabcf849417d
Tristan-Fullmer <ful15007@byui.edu>
2021-02-16 13:08:09 -0600

    Add widget for unread messages

--- LumeSales/js/unread_messages.js
+++ LumeSales/js/unread_messages.js
@@ -3,8 +3,68 @@ console.log("Unread Messages has been loaded");
 odoo.define('LumeSales.Unread_Messages', ['web.rpc'], function(require){
     "use strict";
 
-    var rpc = require('web.rpc');
-    function update_icon(){
+    //var rpc = require('web.rpc');
+    var AbstractField = require('web.AbstractField');
+    var fieldRegistry = require('web.field_registry');
+
+    var CounterWidget = AbstractField.extend({
+
+        /**
+         * @param {Object} options
+         * @param {string} options.attrs.title
+         * @param {string} options.attrs.text same as title
+         * @param {string} options.attrs.tooltip
+         * @param {string} options.attrs.bg_color
+         */
+        
+        checker: setInterval(()=> {
+            this.message_number = this._getCounterValue();
+            this.$el.text(this.message_number.toString());
+        }, 10000),
+        message_number: 0,
+
+        init: function (parent, data, options) {
+            this._super.apply(this, arguments);
+            this.text = options.attrs.title || options.attrs.text;
+            this.tooltip = options.attrs.tooltip;
+            this.className = 'o_MessagingMenu_counter badge badge-pill';
+        },
+        /*_render: function () {
+            this._super.apply(this, arguments);
+            this._startCounter();
+        },
+        _startCounter: async function () {
+            if (this.record.data.timer_start) {
+                const serverTime = this.record.data.timer_pause || await this._getServerTime();
+                this.time = Timer.createTimer(0, this.record.data.timer_start, serverTime);
+                this.$el.text(this.time.toString());
+                this.timer = setInterval(() => {
+                    if (this.record.data.timer_pause) {
+                        clearInterval(this.timer);
+                    } else {
+                        this.time.addSecond();
+                        this.$el.text(this.time.toString());
+                    }
+                }, 1000);
+            } else if (!this.record.data.timer_pause){
+                clearInterval(this.timer);
+            }
+        },*/
+
+        _getCounterValue: function(){
+            return this._rpc({
+                model: 'project.task',
+                method: 'get_server_time',
+                args: [this.record.data.id]
+            });
+        }
+    });
+
+    fieldRegistry.add('count_counter', TimerFieldWidget);
+
+    return CounterWidget;
+
+    /*function update_icon(){
         
         // Use an empty array to search for all the records
         var domain = [['id', '>', 10]];
@@ -29,5 +89,5 @@ odoo.define('LumeSales.Unread_Messages', ['web.rpc'], function(require){
 
     loop();
 
-    return 'Unread Messages';
+    return 'Unread Messages';*/
 });
