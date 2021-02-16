ecf550f7a90098536da48fe9f9408ea23a4ca5d5
Tristan-Fullmer <ful15007@byui.edu>
2021-02-16 14:22:42 -0600

    Update unread_messages.js

--- LumeSales/js/unread_messages.js
+++ LumeSales/js/unread_messages.js
@@ -1,9 +1,7 @@
-//alert("Unread Messages has been loaded");
 console.log("Unread Messages has been loaded");
 odoo.define('LumeSales.Unread_Messages', ['web.AbstractField','web.field_registry'], function(require){
     "use strict";
 
-    //var rpc = require('web.rpc');
     var AbstractField = require('web.AbstractField');
     var fieldRegistry = require('web.field_registry');
 
@@ -22,15 +20,18 @@ odoo.define('LumeSales.Unread_Messages', ['web.AbstractField','web.field_registr
 
         init: function (parent, data, options) {
             this._super.apply(this, arguments);
-            console.log(this.record.data.message_unread_counter);// This is correct
+            //console.log(this.record.data.message_unread_counter);// This is correct
             this.text = this.record.data.message_unread_counter.toString();
             this.className = 'o_MessagingMenu_counter badge badge-pill';
             this.checker = setInterval(async ()=> {
                 //console.log(this.res_id);
                 this.message_number = await this._getCounterValue();
-                console.log(this.message_number);
+                //console.log(this.message_number);
                 this.$el.text(this.message_number.toString());
             }, 10000);
+        },
+        start: function(){
+            this.$el.text(this.record.data.message_unread_counter.toString());
         },
         _getCounterValue: function(){
             return this._rpc({
@@ -45,31 +46,4 @@ odoo.define('LumeSales.Unread_Messages', ['web.AbstractField','web.field_registr
     fieldRegistry.add('count_counter', CounterWidget);
 
     return CounterWidget;
-
-    /*function update_icon(){
-        
-        // Use an empty array to search for all the records
-        var domain = [['id', '>', 10]];
-        // Use an empty array to read all the fields of the records
-        var fields = [];
-        rpc.query({
-            model: 'project.task',
-            method: 'search_read',
-            args: [domain, fields],
-        }).then(function (data) {
-            //console.log(data);
-            // Loop through records
-            // Update associated card
-        });
-    }
-    async function loop(){
-        while (false){
-            update_icon();
-            await new Promise(r => setTimeout(r, 10000));
-        }
-    }
-
-    loop();
-
-    return 'Unread Messages';*/
 });
