a9862f3a4f29559fe8c28227c7a503cec4a01a09
Tristan-Fullmer <ful15007@byui.edu>
2021-02-19 09:08:54 -0600

    try many2many field

--- LumeSales/models/models.py
+++ LumeSales/models/models.py
@@ -108,7 +108,7 @@ class Store(models.Model):
 class User(models.Model):
     _inherit='res.users'
 
-    store = fields.Many2one(comodel_name='lume.store')
+    store = fields.Many2many(comodel_name='lume.store')
 
 class product_addons(models.Model):
     _inherit='product.template'
