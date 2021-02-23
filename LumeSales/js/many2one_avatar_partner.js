4d6059d772ee086c27668be1009752f015f4c26d
Tristan-Fullmer <ful15007@byui.edu>
2021-02-23 09:22:57 -0600

    Update many2one_avatar_partner.js

--- LumeSales/js/many2one_avatar_partner.js
+++ LumeSales/js/many2one_avatar_partner.js
@@ -1,5 +1,5 @@
 console.log("Many2OneAvatarPartner has been loaded");
-odoo.define('lume.Many2OneAvatarPartner', function (require) {
+odoo.define('lume.Many2OneAvatarPartner', ['mail.Many2OneAvatarUser','web.field_registry'], function (require) {
     "use strict";
 
     const fieldRegistry = require('web.field_registry');
@@ -7,19 +7,20 @@ odoo.define('lume.Many2OneAvatarPartner', function (require) {
 
     const Many2OneAvatarPartner = Many2OneAvatarUser.extend({
         // This widget is only supported on many2ones pointing to 'res.users'
-        supportedModels: ['res.user','res.partner'],
+        supportedModels: ['res.users','res.partner'],
     });
 
-    const KanbanMany2OneAvatarPartner = Many2OneAvatarPartner.extend({
-        _template: 'mail.KanbanMany2OneAvatarUser',
-    });
+    // const KanbanMany2OneAvatarPartner = Many2OneAvatarPartner.extend({
+    //     _template: 'mail.KanbanMany2OneAvatarUser',
+    // });
 
     fieldRegistry.add('many2one_avatar_partner', Many2OneAvatarPartner);
-    fieldRegistry.add('kanban.many2one_avatar_partner', KanbanMany2OneAvatarPartner);
+    // fieldRegistry.add('kanban.many2one_avatar_partner', KanbanMany2OneAvatarPartner);
 
-    return {
-        Many2OneAvatarPartner,
-        KanbanMany2OneAvatarPartner,
-    };
+    // return {
+    //     Many2OneAvatarPartner,
+    //     KanbanMany2OneAvatarPartner,
+    // };
+    return Many2OneAvatarPartner;
 
 });
