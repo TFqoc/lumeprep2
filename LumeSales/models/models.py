b754c8eedf1ffc38264792c7e2f6c3c8794e1de0
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-08 12:13:42 -0600

    Update models.py

--- LumeSales/models/models.py
+++ LumeSales/models/models.py
@@ -128,22 +128,16 @@ class sale_inherit(models.Model):
 
     task = fields.Many2one(comodel_name="project.task", readonly=True)
 
-    # @api.onchange('order_line')
-    # def check_order_lines(self):
-    #     for order in self.order_line:
-    #         if order.product_id.is_medical is self.partner_id.is_medical:
-    #             continue
-    #         else:
-    #             #TODO delete the line item that was just added.
-    #             #self.update({'order_line': (3,order.id,0)}) #tries to access id.ref but fails since id is an integer
-    #             return {
-    #             'warning': {'title': "Warning", 'message': "You can't add a " + ("medical" if order.product_id.is_medical else "recreational") + " product to a " + ("medical" if self.partner_id.is_medical else "recreational") + " customer's order!",}
-    #             }
-                #https://github.com/odoo/odoo/issues/32182
-                #Can't edit order_line from here because self is a copy.
-                #Need a work around.
+    @api.onchange('partner_id')
+    def check_order_lines(self):
+        for order in self.order_line:
+            if order.product_id.is_medical is not self.partner_id.is_medical:
+                warning = {
+                'warning': {'title': "Warning", 'message': "You can't set a " + ("medical" if self.partner_id.is_medical else "recreational") + " customer here because is at least one " + ("medical" if order.product_id.is_medical else "recreational") + " product in the order!",}
+                }
+                self.partner_id = False
+                return warning
 
-## TODO Try adding the check for medcial/rec on the sale.order.line object instead
 class sale_line(models.Model):
     _inherit = 'sale.order.line'
 
@@ -154,6 +148,8 @@ class sale_line(models.Model):
                 'warning': {'title': "Warning", 'message': "You can't add a " + ("medical" if self.product_id.is_medical else "recreational") + " product to a " + ("medical" if self.order_id.partner_id.is_medical else "recreational") + " customer's order!",}
                 }
             self.product_id = False
+            self.name = False
+            self.price_unit = False
             return warning
 
 ####
