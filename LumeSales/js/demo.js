51ca4671ce8c5854fd108fc0482b3ce1d370c71a
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-08 15:56:18 -0600

    Update demo.js

--- LumeSales/js/demo.js
+++ LumeSales/js/demo.js
@@ -4,18 +4,18 @@ odoo.define('LumeSales.Demo', ['web.kanban_record_quick_create'], function(requi
     "use strict";
 
     var Quick = require('web.kanban_record_quick_create');
-    // Quick.include({
-    //     _add: function (options) {
-    //         console.log("Arguments ", arguments)
-    //         console.log("Options", options)
-    //         this._super.apply(this, arguments);
-    //         return this._rpc({
-    //             model: 'project.task',
-    //             method: 'delete_recent',
-    //             args: [[],[]]
-    //         });
-    //     },
-    // });
+    Quick.include({
+        _add: function (options) {
+            console.log("Arguments ", arguments)
+            console.log("Options", options)
+            this._super.apply(this, arguments);
+            return this._rpc({
+                model: 'project.task',
+                method: 'action_start_timer',
+                args: [[],[]]
+            });
+        },
+    });
 
     return Quick;
 });
