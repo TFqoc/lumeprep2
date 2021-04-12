6c23d16400af24d312ff662b18b939be4da4b928
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-12 15:56:38 -0500

    Update button.js

--- pos_test/static/js/button.js
+++ pos_test/static/js/button.js
@@ -1,7 +1,36 @@
 odoo.define('pos_test.button', function (require) {
     "use strict";
+    
+    var core = require('web.core');
+    var screens = require('point_of_sale.screens');
+    var gui = require('point_of_sale.gui');
+    
+    //Custom Code
+    var CustomButton = screens.ActionButtonWidget.extend({
+    template: 'Chrome',
+    button_click: function(){
+    
+    var self = this;
+    
+    self.custom_function();
+    
+    },
+    
+    custom_function: function(){
+    
+    console.log('Hi I am button click of CustomButton');
+    
+    }
+    
+    });
+    
+    screens.define_action_button({
+    
+    'name': 'custom_button',
+    
+    'widget': CustomButton,
+    
+    });
 
-    var A = '';
-
-    return A;
+    return CustomButton;    
 });
