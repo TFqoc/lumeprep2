a42fff976d3fd7a3a75cdf05d27c33fe062cae23
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-13 13:17:33 -0500

    Update button.js

--- pos_test/static/js/button.js
+++ pos_test/static/js/button.js
@@ -6,7 +6,7 @@ odoo.define('pos_test.CustomButton', function(require) {
     const { useListener } = require('web.custom_hooks');
     const PosComponent = require('point_of_sale.PosComponent');
     const Registries = require('point_of_sale.Registries');
-    const rpc = require('web.rpc');
+    // const rpc = require('web.rpc');
 
     class CustomButton extends PosComponent {
         constructor() {
@@ -19,7 +19,7 @@ odoo.define('pos_test.CustomButton', function(require) {
         //     return this.env._t(this.state.label);
         // }
         onClick() {
-            result = this.rpc({
+            result = await this.rpc({
                 'model': 'sale.order',
                 'method': 'get_all',
                 // args: [some, args],
