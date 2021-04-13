50ecb8fedbe7854e3d27070e6935db68b972767d
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-13 13:02:42 -0500

    Update button.js

--- pos_test/static/js/button.js
+++ pos_test/static/js/button.js
@@ -1,4 +1,4 @@
-console.log("Button dot js is loaded! Current test: listener for click-product");
+console.log("Button dot js is loaded! Current test: rpc");
 odoo.define('pos_test.CustomButton', function(require) {
     'use strict';
 
@@ -6,6 +6,7 @@ odoo.define('pos_test.CustomButton', function(require) {
     const { useListener } = require('web.custom_hooks');
     const PosComponent = require('point_of_sale.PosComponent');
     const Registries = require('point_of_sale.Registries');
+    const rpc = require('web.rpc');
 
     class CustomButton extends PosComponent {
         constructor() {
