1c2fe27def27e0ef1a45f084b2cecf187a926042
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-13 10:15:54 -0500

    Update button.js

--- pos_test/static/js/button.js
+++ pos_test/static/js/button.js
@@ -3,20 +3,26 @@ odoo.define('pos_test.CustomButton', function(require) {
     'use strict';
 
     const { useState } = owl;
+    const { useListener } = require('web.custom_hooks');
     const PosComponent = require('point_of_sale.PosComponent');
     const Registries = require('point_of_sale.Registries');
 
     class CustomButton extends PosComponent {
         constructor() {
             super(...arguments);
-            this.state = useState({ label: 'Click Me!' });
+            useListener('click-order', this.onAddProduct);
+            // this.state = useState({ label: 'Click Me!' });
             // this.confirmed = null;
         }
-        get translatedLabel() {
-            return this.env._t(this.state.label);
-        }
+        // get translatedLabel() {
+        //     return this.env._t(this.state.label);
+        // }
         onClick() {
             console.log("You clicked me!");
+        }
+        onAddProduct({ detail: clickedOrder }){
+            console.log("You just added a product!");
+            console.log(clickedOrder);
         }
     }
     CustomButton.template = 'CustomButton';
