28cd7fd29e38b3308f99d60e35f0cc6676a37bfc
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-13 13:41:10 -0500

    Update patch.js

--- pos_test/static/js/patch.js
+++ pos_test/static/js/patch.js
@@ -4,6 +4,7 @@ odoo.define('pos_test.PatchTest', function(require) {
 
     const { patch } = require("web.utils");
     var ProductScreen = require("point_of_sale.ProductScreen");
+    var NumberBuffer = require("point_of_sale.NumberBuffer");
 
     patch(ProductScreen, "log message", {
         async _clickProduct(event) {
@@ -11,6 +12,16 @@ odoo.define('pos_test.PatchTest', function(require) {
             // do things
             console.log("You clicked on " + event.detail.display_name);
             console.log(event);
+        },
+      });
+
+      patch(NumberBuffer, "log delete", {
+        _updateBuffer(input) {
+            this._super(...arguments);
+            // do things
+            if (input === "Backspace"){
+                console.log("Backspace was clicked");
+            }
         },
       });
 
