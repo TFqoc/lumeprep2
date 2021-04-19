4b3eb20fa95c273b42e0f4e3b67e010048a6274a
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-19 09:04:54 -0500

    Update update_orders.js

--- pos_test/static/js/update_orders.js
+++ pos_test/static/js/update_orders.js
@@ -5,7 +5,7 @@ odoo.define('pos_test.UpdateOrders', function(require) {
     const PosComponent = require('point_of_sale.PosComponent');
     const TicketButton = require('point_of_sale.TicketButton');
     const Registries = require('point_of_sale.Registries');
-    import { useRef } from "owl/hooks";
+    const { useRef } = owl.hooks;
 
     class UpdateOrders extends PosComponent {
         constructor(){
@@ -14,8 +14,11 @@ odoo.define('pos_test.UpdateOrders', function(require) {
         }
         mounted() {
             console.log("Update is mounting");
-            this.getOrders();
+            //this.getOrders();
             this.getter = setInterval(this.getOrders.bind(this), 10000);
+        }
+        async willStart(){
+            await this.getOrders();
         }
         async getOrders(){
                 var linked_sale_order_ids = [];
@@ -32,15 +35,11 @@ odoo.define('pos_test.UpdateOrders', function(require) {
                     'method': 'get_orders',
                     args: [linked_sale_order_ids, this.env.pos.pos_session.id],
                     // Pass session_id, session object has reference to config object
-                    // Pass list of so ids from current orders
-                    // Pass this.pos.pos_session.id
-                    // this.config.id (pos shop id)
-                    // this.config.current_session_id (pos.session id)
                 }).then((result) => {
                     // TODO check returned orders against what we have.
                     //console.log("I got these sale orders: " + result);
                     this.env.pos.import_orders(result);
-                    this.ticketRef.render();
+                    this.ticketRef.comp.render();
                 },
                 (args) => {
                     console.log("Failed to get new orders from backend.");
