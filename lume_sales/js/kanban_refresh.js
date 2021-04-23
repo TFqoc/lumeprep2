9b4f565df9eff25ab6894880559ca3e0d05ba87b
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-23 14:26:15 -0500

    refresh kanban views

---
+++ lume_sales/js/kanban_refresh.js
@@ -0,0 +1,18 @@
+console.log("KanbanRefresh has been loaded");
+odoo.define('lume_sales.kanban_refresh', function(require){
+    "use strict";
+
+    var KanbanRenderer = require('web.KanbanRenderer');
+    KanbanRenderer.include({
+        init: function (parent, state, params) {
+            this._super.apply(this, arguments);
+            this.refresh = setInterval(this._renderView, 10000);
+        },
+        _renderView_debug: function(){
+            this._renderView();
+            console.log("Re-rendering");
+        },
+    });
+
+    return KanbanRenderer;
+});
