39c439ce3c3eb8626b0eca77b7a4339ebbab48df
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-26 15:22:45 -0500

    Update kanban_refresh.js

--- lume_sales/js/kanban_refresh.js
+++ lume_sales/js/kanban_refresh.js
@@ -2,17 +2,30 @@ console.log("KanbanRefresh has been loaded");
 odoo.define('lume_sales.kanban_refresh', function(require){
     "use strict";
 
-    var KanbanRenderer = require('web.KanbanRenderer');
-    KanbanRenderer.include({
-        init: function (parent, state, params) {
-            this._super.apply(this, arguments);
-            // this.refresh = setInterval(this._renderView.bind(this), 10000);
+    const { patch } = require("web.utils");
+    const SearchBar = require("web.SearchBar");
+
+    patch(SearchBar, "Refresh search every 10 seconds", {
+        mounted() {
+            this._super(...args)
+            setInterval(this.refresh.bind(this), 10000);
         },
-        _renderView_debug: function(){
-            // this._renderView();
-            console.log("Re-rendering (commented)");
-        },
+        refresh(){
+            this.model.dispatch('search');
+        }
     });
 
-    return KanbanRenderer;
+    // var KanbanRenderer = require('web.KanbanRenderer');
+    // KanbanRenderer.include({
+    //     init: function (parent, state, params) {
+    //         this._super.apply(this, arguments);
+    //         // this.refresh = setInterval(this._renderView.bind(this), 10000);
+    //     },
+    //     _renderView_debug: function(){
+    //         // this._renderView();
+    //         console.log("Re-rendering (commented)");
+    //     },
+    // });
+
+    // return KanbanRenderer;
 });
