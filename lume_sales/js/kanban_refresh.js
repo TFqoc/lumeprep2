b8e7b264835bad0a77233f11254d04682c973f61
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-27 14:20:32 -0500

    Update kanban_refresh.js

--- lume_sales/js/kanban_refresh.js
+++ lume_sales/js/kanban_refresh.js
@@ -1,19 +1,61 @@
-console.log("KanbanRefresh has been loaded");
+console.log("KanbanRefresh has been loaded. Testing pause the reload. messages");
 odoo.define('lume_sales.kanban_refresh', function(require){
     "use strict";
 
     const { patch } = require("web.utils");
     const SearchBar = require("web.SearchBar");
+    const { bus } = require("web.core");
+    var QuickCreate = require('web.kanban_record_quick_create');
 
     patch(SearchBar, "Refresh search every 10 seconds", {
         mounted() {
-            this._super(...args)
-            setInterval(this.refresh.bind(this), 10000);
+            this._super(...arguments);
+            setInterval(this.refresh.bind(this), 15000);
+            bus.on('quick_create_start', this, this.pause);
+            bus.on('quick_create_end', this, this.unpause);
+            this.paused = false;
         },
         refresh(){
-            this.model.dispatch('search');
+            if (!this.paused){
+                this.model.dispatch('search');
+                console.log("Reloading");
+            }
+            else{
+                console.log("Not Reloading");
+            }
+        },
+        pause(){
+            this.paused = true;
+            console.log("Trigger start caught");
+        },
+        unpause(){
+            this.paused = false;
+            console.log("Trigger end caught");
         }
     });
+
+    QuickCreate.include({
+        init: function (options) {
+            this._super.apply(this, arguments);
+            bus.trigger('quick_create_start');
+            console.log("Trigger start");
+        },
+        _add: function (options) {
+            this._super.apply(this, arguments);
+            bus.trigger('quick_create_end');
+            console.log("Trigger end");
+        },
+        _cancel: function () {
+            this._super.apply(this, arguments);
+            bus.trigger('quick_create_end');
+            console.log("Trigger ends");
+        },
+    });
+    
+    return QuickCreate;
+
+    // Somewhere else
+    // bus.trigger();
 
     // var KanbanRenderer = require('web.KanbanRenderer');
     // KanbanRenderer.include({
