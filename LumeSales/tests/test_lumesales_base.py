328f4a1cbc965c3f262184ac9b158ff07934cb10
JMC-ai <80903570+JMC-ai@users.noreply.github.com>
2021-04-28 17:01:39 -0500

    Update test_lumesales_base.py

--- LumeSales/tests/test_lumesales_base.py
+++ LumeSales/tests/test_lumesales_base.py
@@ -16,34 +16,84 @@ class TestLumeSaleCommon(SavepointCase):
         Users = cls.env['res.users'].with_context({'no_reset_password': True})
 
         cls.user_receptionist = Users.create({
-            'Name' : 'Pete Zeria'
+            'Name': 'Pete Zeria'
+            'groups_id': [(6, 0, [cls.env.ref('base.group_public').id])]})
         })
 
-        cls.user_luminary = Users.create({})
+        cls.user_luminary = Users.create({
+            'Name': 'Justin Case'
+        })
 
-        cls.user_picking = Users.create({})
+        cls.user_picking = Users.create({
+            'Name': 'Adam Zaple'
+        })
 
-        cls.user_cashier = Users.create({})
+        cls.user_cashier = Users.create({
+            'Name': 'Robin Banks'
+        })
 
-        cls.user_manager = Users.create({})
+        cls.user_manager = Users.create({
+            'Name': 'Ella Vader'
+        })
 
-        cls.user_district_manager = Users.create({})
+        cls.user_district_manager = Users.create({
+            'Name': 'Adam Sandler'
+        })
 
         #Creating Stores:
 
         cls.lumestore_one = cls.env['project.project'].with_context({'mail_create_nolog': True}).create({
             'name': 'Peterson',
             'privacy_visibility': 'followers',
-            'alias_name': 'project+goats',
+            'alias_name': 'project+peterson',
             'partner_id': cls.partner_1.id,
             'type_ids': [
                 (0, 0, {
-                    'name': 'New',
+                    'name': 'Check In',
                     'sequence': 1,
                 }),
                 (0, 0, {
-                    'name': 'Won',
+                    'name': 'Build Cart',
                     'sequence': 10,
+                }),
+                (0, 0, {
+                    'name': 'Fulfillment',
+                    'sequence': 20,
+                }),
+                (0, 0, {
+                    'name': 'Order Ready',
+                    'sequence': 30,
+                }),
+                (0, 0, {
+                    'name': 'Done',
+                    'sequence': 400,
+                })]
+            })
+        cls.lumestore_two = cls.env['project.project'].with_context({'mail_create_nolog': True}).create({
+            'name': 'Escanaba',
+            'privacy_visibility': 'followers',
+            'alias_name': 'project+escanaba',
+            'partner_id': cls.partner_1.id,
+            'type_ids': [
+                (0, 0, {
+                    'name': 'Check In',
+                    'sequence': 1,
+                }),
+                (0, 0, {
+                    'name': 'Build Cart',
+                    'sequence': 10,
+                }),
+                (0, 0, {
+                    'name': 'Fulfillment',
+                    'sequence': 20,
+                }),
+                (0, 0, {
+                    'name': 'Order Ready',
+                    'sequence': 30,
+                }),
+                (0, 0, {
+                    'name': 'Done',
+                    'sequence': 400,
                 })]
             })
 
