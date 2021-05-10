5618387dd6d864a278b47645a9d5a2c0315450c3
JMC-ai <80903570+JMC-ai@users.noreply.github.com>
2021-05-10 08:59:40 -0700

    Added Logger
    Added logging information to Lume Customer and the Test Base.

--- lume_sales/tests/test_lume_customer.py
+++ lume_sales/tests/test_lume_customer.py
@@ -9,15 +9,18 @@ class TestComputedFields(TestLumeSaleCommon):
             self.customer_banned.is_banned, 
             "Error in is_banned computed field: Customer was not banned despite having three or more strikes."
         )
+        _logger.info("Test Is Banned Status: Complete.")
 
     def test_compute_21(self):
         self.assertTrue(
             self.customer_rec.is_over_21,
             "Error in compute_21 computed field: Customer does not register as over age despite being far older."
         )
-        
+        _logger.info("Test Compute Twenty-One Status: Complete.")
+
     def test_lume_testing(self):
         self.assertTrue(False, "This should always fail.")
+        _logger.info("Lume Tests are Running.")
 
 class TestCustomerProfile(TestLumeSaleCommon):
     def test_warn(self):
@@ -27,5 +30,6 @@ class TestCustomerProfile(TestLumeSaleCommon):
             1, 
             "Error in Partner Model: Number of warnings does not increase with the current method."
         )
+        _logger.info("Test Warn Status: Complete.")
     
     
