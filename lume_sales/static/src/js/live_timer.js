3da76195308ec19313dba8b12aefa9ff27125853
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-05-12 08:33:59 -0500

    comment cleanup

--- lume_sales/static/src/js/live_timer.js
+++ lume_sales/static/src/js/live_timer.js
@@ -1,25 +1,4 @@
-//alert("Unread Messages has been loaded");
 console.log("Live Timer has been loaded");
-// odoo.define('LumeSales.live_timer', function(require){
-//     "use strict";
-
-//     var Timer_timer = require('timer.timer');
-//     var fieldRegistry = require('web.field_registry');
-
-//     var LiveTimer = Timer_timer.extend({
-
-//         init: function (parent, name, record, options) {
-//             this._super.apply(this, arguments);
-//             this.className = 'o_field_widget o_readonly_modifier text-danger ml-auto h5 ml-4 font-weight-bold';
-//         },
-
-//     });
-
-//     fieldRegistry.add('LumeSales.live_timer', LiveTimer);
-
-//     return LiveTimer;
-// });
-
 odoo.define('timer.live_timer', function (require) {
     "use strict";
     
