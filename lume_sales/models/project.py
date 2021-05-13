4f84e22f1a891042aec9419be602acc48f7ec2f5
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-05-13 12:57:19 -0500

    Update project.py

--- lume_sales/models/project.py
+++ lume_sales/models/project.py
@@ -109,7 +109,7 @@ class Tasks(models.Model):
                 qty = 0
                 for line in record.sales_order.order_line:
                     qty += line.product_uom_qty
-                record.monetary_display = "%s Qty: %s" % (str(record.sales_order.amount_total), str(qty))
+                record.monetary_display = "$%.2f Qty: %s" % (record.sales_order.amount_total, str(qty))
             else:
                 record.monetary_display = False
 
