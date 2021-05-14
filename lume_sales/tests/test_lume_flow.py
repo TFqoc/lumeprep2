fa49be4cccab494b7c624bdc3f74dbf752fbba24
JMC-ai <80903570+JMC-ai@users.noreply.github.com>
2021-05-14 06:03:05 -0700

    Update test_lume_flow.py

--- lume_sales/tests/test_lume_flow.py
+++ lume_sales/tests/test_lume_flow.py
@@ -24,9 +24,9 @@ class TestLumeTaskPosition(TestLumeSaleCommon):
         })
         record_ids = [Test_Task.id]
         # TODO: This needs to be set to be the id of the current row's record.
-        active_id = Test_Task.id
+        active_id = [Test_Task.id]
         # TODO: This needs to be all models currently loaded in the wizard
-        active_ids = Test_Task.id
+        active_ids = [self.lumestore_one.id]
 
         uid = self.env.ref('base.user_admin').id
         self.env['project.task'].browse(record_ids).with_context({
