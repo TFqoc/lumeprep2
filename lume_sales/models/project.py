aaf0084acd58c6848679a9bb2faf0e481f3bdbd1
moneill-qoc <moneill@qocinnovations.com>
2021-05-16 23:30:58 -0500

    Scan DL from mobile device

--- lume_sales/models/project.py
+++ lume_sales/models/project.py
@@ -397,8 +397,8 @@ class project_tasks_inherit(models.Model):
     def _adjust_image(self):
         for record in self:
             _logger.info("In _adjust_image")
-            _logger.info(str(record.DL_or_med_image))
-            if str(record.DL_or_med_image) is not False:
+            _logger.info("DL Record:" + str(record.DL_or_med_image))
+            if record.DL_or_med_image is not False:
                 image = tools.base64_to_image(record.DL_or_med_image)
                 _logger.info("Image type:" + str(type(image)))
                 _logger.info("Image size:" + str(image.size))
