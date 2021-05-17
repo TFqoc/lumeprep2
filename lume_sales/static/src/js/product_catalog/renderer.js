40442544cf111c7d0ba041f198776cc8e3a98809
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-05-17 11:21:18 -0500

    Update renderer.js

--- lume_sales/static/src/js/product_catalog/renderer.js
+++ lume_sales/static/src/js/product_catalog/renderer.js
@@ -1,9 +1,9 @@
 odoo.define('lume_sales.ProductKanbanRenderer', function (require) {
     "use strict";
     
-    const KanbanRenderer = require('web.KanbanView');
+    const KanbanRenderer = require('web.KanbanRenderer');
 
-    return LPCRenderer = KanbanRenderer.extend({
+    return KanbanRenderer.extend({
         _renderView: function(){
             var self = this;
             return this._super.apply(this, arguments).then(function () {
