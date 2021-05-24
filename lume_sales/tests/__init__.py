2073599e8b13a66d6f4e5928b80da53a3be4b8f3
JMC-ai <80903570+JMC-ai@users.noreply.github.com>
2021-05-24 10:39:16 -0700

    Update __init__.py

--- lume_sales/tests/__init__.py
+++ lume_sales/tests/__init__.py
@@ -2,4 +2,4 @@ from . import test_lumesales_base
 #from . import test_lume_checkin 
 from . import test_lume_customer
 #from . import test_lume_so
-from . import test_lume_flow
+#from . import test_lume_flow
