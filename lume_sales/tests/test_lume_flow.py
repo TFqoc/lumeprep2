c6256779f3ee1239c0cb56d7830ae6007fd0010c
JMC-ai <80903570+JMC-ai@users.noreply.github.com>
2021-05-24 12:54:06 -0700

    Update test_lume_flow.py

--- lume_sales/tests/test_lume_flow.py
+++ lume_sales/tests/test_lume_flow.py
@@ -11,6 +11,31 @@ _logger = logging.getLogger(__name__)
 
 @tagged('lume')
 class TestRecLumeFlow(TestLumeSaleCommon):
+    def test_onchange(self):
+        record_ids = []
+        active_id = self.lumestore_one.id
+        active_ids = [self.lumestore_one.id]
+        uid = self.env.ref('base.user_admin').id
+        self.env['project.task'].browse(record_ids).with_context({
+            'active_id': active_id,
+            'active_ids': active_ids,
+            'active_model': 'project.project',
+            'allowed_company_ids': [1],
+            'default_project_id': 5,
+            'default_stage_id': 5,
+            'lang': 'en_US',
+            'pivot_row_groupby': ['user_id'],
+            'tz': 'Europe/Brussels',
+            'uid': uid}).with_user(uid).onchange({
+                'scan_text': '@ANSI 636032030102DL00410205ZM03460027DLDCADCBDCDDBA12312021DCSLOVEDCTEVE ADBDDBB02171987DBC2DAYDAUDAG629 MAD DOG LANEDAIDETROITDAJMIDAK482010001  DAQC 333 547 393 957DCFDCGUSADCHDAHDCKDDAN', 
+                'partner_id': False, 
+                'fulfillment_type': 'store', 
+                'order_type': 'adult', 
+                'project_id': self.lumestore_one.id, 
+                'timesheet_product_id': False, 
+                'company_id': 1, 
+                'parent_id': False}, 'scan_text', {'scan_text': '1', 'partner_id': '1', 'fulfillment_type': '', 'order_type': '', 'project_id': '1', 'timesheet_product_id': '', 'company_id': '1', 'parent_id': '1'})
+
     def test_task_to_build_cart(self): #Upon pressing build cart, the tile should be moved to the Build Cart Stage.
         Task = self.env['project.task'].with_context({'tracking_disable': True})
         uid = self.env.ref('base.user_admin').id
