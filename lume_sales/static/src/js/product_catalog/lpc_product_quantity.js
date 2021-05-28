cb6abecb3429f4ccbc9ae1a862c2a20ec9441676
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-05-28 12:26:06 -0500

    added thc_type as a data attribute on the card

--- lume_sales/static/src/js/product_catalog/lpc_product_quantity.js
+++ lume_sales/static/src/js/product_catalog/lpc_product_quantity.js
@@ -151,6 +151,9 @@ function update_data() {
             success: function (data) {
                 $("#TOTAL").text(`Total: $${data.result[0].toFixed(2)}`);
                 $("#QTY").text(`Quantity: ${data.result[1].toFixed(1)}`);
+                let dataType = data.result[2];
+                $("div.o_kanban_card").removeClass("catalog_card_hide");
+                $(`div.o_kanban_card[data-type='${dataType}']`).addClass("catalog_card_hide");
             },
             dataType: 'json',
             type: 'POST',
