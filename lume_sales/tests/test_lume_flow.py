f82f619349d3af3f1a7c45d830137da9d06b2afa
JMC-ai <80903570+JMC-ai@users.noreply.github.com>
2021-06-02 11:23:41 -0500

    Update test_lume_flow.py

--- lume_sales/tests/test_lume_flow.py
+++ lume_sales/tests/test_lume_flow.py
@@ -13,65 +13,6 @@ _logger = logging.getLogger(__name__)
 
 @tagged('lume') 
 class TestRecLumeFlow(TestLumeSaleCommon):
-    def test_checkin_onchange(self):
-        _logger.warning("Product Rec's Type is %s" % self.product_rec.type)
-        Task = self.env['project.task'].with_context({'tracking_disable': True})
-        Test_Task = Task.create({
-            'name': 'Test',
-            'project_id': self.lumestore_one.id,
-        })
-        _logger.warning(Test_Task.partner_id)
-        record_ids = [Test_Task.id]
-        active_id = Test_Task.id
-        active_ids = [self.lumestore_one.id, Test_Task.id]
-        uid = self.env.ref('base.user_admin').id
-        self.env['project.task'].browse(record_ids).with_context({
-            'active_id': active_id,
-            'active_ids': active_ids,
-            'active_model': 'project.project',
-            'allowed_company_ids': [1],
-            'default_project_id': 5,
-            'default_stage_id': self.env.ref('lume_sales.lume_stage_0'),
-            'lang': 'en_US',
-            'pivot_row_groupby': ['user_id'],
-            'tz': 'Europe/Brussels',
-            'uid': uid}).with_user(uid).onchange({
-                'scan_text': '@ANSI 636032030102DL00410205ZM03460027DLDCADCBDCDDBA12312021DCSLOVEDCTEVE ADBDDBB02171987DBC2DAYDAUDAG629 MAD DOG LANEDAIDETROITDAJMIDAK482010001  DAQC 333 547 393 957DCFDCGUSADCHDAHDCKDDAN', 
-                'partner_id': False, 
-                'fulfillment_type': 'store', 
-                'order_type': 'adult', 
-                'project_id': self.lumestore_one.id, 
-                #'timesheet_product_id': False, 
-                'company_id': 1, 
-                'parent_id': False}, 
-                'scan_text', {
-                    'scan_text': '1', 
-                    'partner_id': '1', 
-                    'fulfillment_type': '', 
-                    'order_type': '', 
-                    'project_id': '1', 
-                    'timesheet_product_id': '', 
-                    'company_id': '1', 
-                    'parent_id': '1'})
-        # TODO Assert statements.
-        self.assertFalse(
-            Test_Task.scan_text,
-            "Error in Check In Onchange: Scan Text field was %s instead of False." % (Test_Task.scan_text)
-        )
-
-        _logger.warning(Test_Task.partner_id)
-        _logger.warning(Test_Task.fulfillment_type)
-        _logger.warning(Test_Task.order_type)
-        _logger.warning(Test_Task.project_id)
-
-        # TODO Verify what is being returned and how the data from the barcode parse is passed on to the check in screen.
-
-        # self.assertEqual(
-        #     Test_Task.partner_id.id, #This is not being set correctly.
-        #     self.customer_rec.id,
-        #     "Error in Check In Onchange: Partner Id was %s instead of %s" % (Test_Task.partner_id, self.customer_rec)
-        # )
-
     def test_scan_text(self):
         Task = self.env['project.task'].with_context({'tracking_disable': True})
         Test_Task = Task.create({
@@ -85,6 +26,11 @@ class TestRecLumeFlow(TestLumeSaleCommon):
             Test_Task.partner_id.id, #This is not being set correctly.
             self.customer_rec.id,
             "Error in Check In Onchange: Partner Id was %s instead of %s" % (Test_Task.partner_id, self.customer_rec)
+        )
+
+        self.assertFalse(
+            Test_Task.scan_text,
+            "Error in Auto_Fill: Scan Text field was not emptied."
         )
 
     def test_barcode_parse(self): 
