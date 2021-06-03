cd740eda40027ba7cd713896fce2204a7ca0168f
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-06-03 15:36:55 -0500

    Update models.py

--- lume_sales/models/models.py
+++ lume_sales/models/models.py
@@ -244,7 +244,7 @@ class Picking(models.Model):
 
     # Quick endpoint for locust testing
     def quick_validate(self):
-        # ctx = {"skip_immediate":True,"skip_backorder":True}
-        # self.with_context(skip_immediate=True,skip_backorder=True).button_validate()
-        self._action_done()
+        for line in self.move_ids_without_package:
+            line.quantity_done = line.product_uom_qty
+        self.button_validate()
         return True
