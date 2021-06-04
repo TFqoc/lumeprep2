42ddb99c6ebd847e105b6604ca276839b21bf978
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-06-04 11:23:33 -0500

    qty widget update

--- lume_sales/static/src/js/qty_widget.js
+++ lume_sales/static/src/js/qty_widget.js
@@ -1,4 +1,4 @@
-console.log("QTY widget has been loaded");
+console.log("QTY widget has been loaded 7");
 odoo.define('lume_sales.qty_widget', function (require) {
     "use strict";
     
@@ -6,16 +6,23 @@ odoo.define('lume_sales.qty_widget', function (require) {
     var Widget = require('web.Widget');
     var FieldFloat = require('web.basic_fields').FieldFloat;
     
-    var QTYWidget = Widget.extend({
+    var QTYWidget = FieldFloat.extend({
         template: 'qty_template',
         init: function (parent,value) {
             this._super.apply(this, arguments);
-            this.className = 'o_field_widget o_readonly_modifier timer-normal ml-auto h5 ml-4 font-weight-bold';
+//             this.className = 'o_field_widget o_readonly_modifier timer-normal ml-auto h5 ml-4 font-weight-bold';
         },
         start: function(){
             // Rendering is done so bind methods to the buttons
-            this.$el;
-            // return promise;
+            this.$el.children().first().click(this.subtractQty.bind(this));
+            this.$el.children().last().click(this.addQty.bind(this));
+            return Promise.resolve();
+        },
+        addQty: function(){
+            this._setValue((this.value + 1) + "", {});
+        },
+        subtractQty: function(){
+            this._setValue((this.value - 1) + "", {});
         }
         
     });
