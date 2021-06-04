49aca4af392718bd79ed5235d9069fe2c9d5c764
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-06-04 10:37:26 -0500

    start qty widget

---
+++ lume_sales/static/src/js/qty_widget.js
@@ -0,0 +1,25 @@
+console.log("QTY widget has been loaded");
+odoo.define('lume_sales.qty_widget', function (require) {
+    "use strict";
+    
+    var fieldRegistry = require('web.field_registry');
+    var Widget = require('web.Widget');
+    var FieldFloat = require('web.basic_fields').FieldFloat;
+    
+    var QTYWidget = Widget.extend({
+        template: 'qty_template',
+        init: function (parent,value) {
+            this._super.apply(this, arguments);
+            this.className = 'o_field_widget o_readonly_modifier timer-normal ml-auto h5 ml-4 font-weight-bold';
+        },
+        start: function(){
+            // Rendering is done so bind methods to the buttons
+            this.$el;
+            // return promise;
+        }
+        
+    });
+    
+    fieldRegistry.add('qty_widget', QTYWidget);
+    
+    });
