12d7766eeacf508548fae410bc5d01dabe852edc
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-06-07 14:48:11 -0500

    Update note_wizard.py

--- lume_sales/wizards/note_wizard.py
+++ lume_sales/wizards/note_wizard.py
@@ -5,7 +5,7 @@ class NoteWizard(models.TransientModel):
     _description = 'A wizard for displaying notes'
 
     partner_id = fields.Many2one('res.partner', readonly=True)
-    note_ids = fields.One2many(compute='_compute_notes')
+    note_ids = fields.One2many(comodel_name='lume.note',compute='_compute_notes')
     message = fields.Char('Message', required=True)
 
     #@api.multi
