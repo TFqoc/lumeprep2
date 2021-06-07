495fa9f967afbae1700c92e38840cd4c1f095e7a
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-06-07 13:29:53 -0500

    add access rules

--- lume_sales/__manifest__.py
+++ lume_sales/__manifest__.py
@@ -29,7 +29,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'wizards/scan_dl.xml',
         'wizards/message_wizard.xml',
         'wizards/note_wizard.xml',
