6c51952eebab8337d5ca24e78b17c55b147cecbc
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-06-08 10:36:42 -0500

    typeo

--- lume_sales/models/note.py
+++ lume_sales/models/note.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 class Note(models.Model):
     _name = 'lume.note'
-    _order = 'date_create'
+    _order = 'create_date'
 
     logged_partner_id = fields.Many2one('res.partner')
     source_partner_id = fields.Many2one('res.partner')
