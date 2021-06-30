9e6345ade6270b7c5ab3beb73a95306928193876
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-06-30 09:57:32 -0500

    Update settings.py

--- lume_sales/models/settings.py
+++ lume_sales/models/settings.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api
 
-class Settings(models.Model):
+class Settings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     batch_threshold = fields.Integer(string="Batch Threshold")
