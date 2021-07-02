56700decdb739c8afd653d78707dc4f073c2d9ed
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-07-02 15:36:46 -0500

    add comment

--- lume_sales/static/src/js/progressbar.js
+++ lume_sales/static/src/js/progressbar.js
@@ -7,6 +7,7 @@ odoo.define('lume_sales.KanbanProgressBar', function (require) {
     ProgressBar.include({
         init: function (parent, options, columnState) {
             this.colors = _.extend({}, columnState.progressBarValues.colors, {
+                // These colors got changed in the scss file, so these names are legacy now
                 store: 'steelblue',
                 online: 'goldenrod',
                 curb: 'firebrick',
