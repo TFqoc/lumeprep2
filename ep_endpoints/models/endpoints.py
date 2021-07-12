c2f6ff705ef072f571daab29603179718b9fe756
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-07-12 15:15:27 -0500

    Update endpoints.py

--- ep_endpoints/models/endpoints.py
+++ ep_endpoints/models/endpoints.py
@@ -1,5 +1,26 @@
 from odoo import models, fields, api
 import datetime
+import json
+
+def jsonify_records(records, fields=False):
+    data = []
+    # Validate fields
+    if fields:
+        try:
+            for field in fields:
+                records[field]
+        except:
+            return "{'error': 'Bad field name'}"
+        if 'id' not in fields:
+            fields.append('id')
+    for r in records:
+        filter_fields = fields or records.fields_get()
+        record = {}
+        for f in filter_fields:
+            record[f] = r[f]
+        data.append(record)
+    return json.dumps(data)
+
 
 class Product(models.Model):
     _inherit = 'product.product'
@@ -8,24 +29,16 @@ class Product(models.Model):
     def ep_hourly_products(self, fields=False):
         hour_ago = datetime.datetime.now() - datetime.timedelta(hours=1)
         data = self.env['product.product'].search([('__last_update','>',hour_ago)])
-        if fields:
-            l = []
-            try:
-                for field in fields:
-                    l.append(data[field])
-                return l
-            except:
-                return "{'error': 'Bad field name'}"
-        return data
+        return jsonify_records(data, fields)
 
     @api.model
-    def ep_nightly_products(self):
+    def ep_nightly_products(self, fields=False):
         data = self.env['product.product'].search([('active','=',True)])
         extra_data = self.env['product.product'].search([('active','=',False)])
         lots = self.env['stock.lot'].search([]).product_id
         extra_data = extra_data.filtered(lambda x: x in lots)
         data |= extra_data
-        return data
+        return jsonify_records(data, fields)
 
 
 class Lots(models.Model):
