52f92af9183a2c86d7680cc2a1406e07dae3b64e
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-07-19 13:53:55 -0500

    tier selection on lot only

--- lume_sales/models/stock_lot.py
+++ lume_sales/models/stock_lot.py
@@ -21,6 +21,7 @@ class StockLot(models.Model):
     stock_at_store = fields.Float(compute="_compute_stock_at_store")
     lpc_quantity = fields.Integer('Product Quantity', compute="_compute_lpc_quantity", inverse="_inverse_lpc_quantity")
 
+    is_tiered = fields.Boolean(related="product_id.is_tiered")
     tier = fields.Selection([('top','Top'),('mid','Mid'),('value','Value'),('cut','Fresh Cut')])
 
     @api.depends('product_id.categ_id','product_id.effect','product_id.brand')
--- lume_sales/models/stock_quant.py
+++ lume_sales/models/stock_quant.py
@@ -4,14 +4,4 @@ from odoo import models, fields, api
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    tier = fields.Selection(selection=[('top','Top'),('mid','Mid'),('value','Value'),('cut','Fresh Cut')],
-        compute="_compute_tier",inverse="_inverse_tier",readonly=False,store=True)
-    is_tiered = fields.Boolean(related="product_id.is_tiered")
-
-    def _compute_tier(self):
-        for record in self:
-            record.tier = record.lot_id.tier
-
-    def _inverse_tier(self):
-        for record in self:
-            record.lot_id.tier = record.tier
+    tier = fields.Selection(related="lot_id.tier")
