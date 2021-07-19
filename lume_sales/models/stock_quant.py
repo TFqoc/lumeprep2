561189312dc8a3e26eb78827d22ee0a3c73ab0b0
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-07-19 13:27:17 -0500

    Update stock_quant.py

--- lume_sales/models/stock_quant.py
+++ lume_sales/models/stock_quant.py
@@ -4,5 +4,5 @@ from odoo import models, fields, api
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    tier = fields.Selection(related="lot_id.tier")
-    is_tiered = fields.Boolean(related="product_id.is_tiered",store=True,readonly=False)
+    tier = fields.Selection(related="lot_id.tier",store=True,readonly=False)
+    is_tiered = fields.Boolean(related="product_id.is_tiered")
