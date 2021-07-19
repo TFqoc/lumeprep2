eebffdf89ce005ee6a50dca4423c33f4ffa5d899
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-07-19 14:11:10 -0500

    edit tier from quant

--- lume_sales/models/stock_quant.py
+++ lume_sales/models/stock_quant.py
@@ -4,4 +4,18 @@ from odoo import models, fields, api
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
+    is_tiered = fields.Boolean(compute="_compute_tiered")
     tier = fields.Selection(related="lot_id.tier")
+
+    def _compute_tiered(self):
+        for record in self:
+            if record.product_id and record.lot_id:
+                record.is_tiered = record.product_id.is_tiered
+                record.tier = record.lot_id.tier
+            elif record.product_id:
+                record.is_tiered = record.product_id.is_tiered
+
+    @api.onchange('tier')
+    def _change_tier(self):
+        if self.lot_id:
+            self.lot_id.tier = self.tier
