7a2ad7a284fe428e132f02650041731245d73a75
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-07-19 13:18:03 -0500

    store related field

--- lume_sales/models/stock_quant.py
+++ lume_sales/models/stock_quant.py
@@ -5,4 +5,4 @@ class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
     tier = fields.Selection(related="lot_id.tier")
-    is_tiered = fields.Boolean(related="product_id.is_tiered")
+    is_tiered = fields.Boolean(related="product_id.is_tiered",store=True)
