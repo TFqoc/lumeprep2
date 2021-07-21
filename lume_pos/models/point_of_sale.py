f7684d90307f2c499140d92f4b392aab20a7d97d
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-07-21 13:51:41 -0500

    Update point_of_sale.py

--- lume_pos/models/point_of_sale.py
+++ lume_pos/models/point_of_sale.py
@@ -7,3 +7,18 @@ class PointOfSale(models.Model):
 
     project_id = fields.Many2one('project.project')
     store_name = fields.Char(related="project_id.name", store=True)
+
+    def _default_pricelist(self):
+        return super()._default_pricelist()
+    # Override
+    pricelist_id = fields.Many2one('product.pricelist', string='Default Pricelist', required=True, default=_default_pricelist,
+        help="The pricelist used if no customer is selected or if the customer has no Sale Pricelist configured.",
+        compute="_get_store_pricelist",store=True)
+
+    @api.depends('project_id.store_pricelist')
+    def _get_store_pricelist(self):
+        for record in self:
+            if record.project_id:
+                record.pricelist_id = record.project_id.store_pricelist or record._default_pricelist()
+            else:
+                record.pricelist_id = record._default_pricelist()
