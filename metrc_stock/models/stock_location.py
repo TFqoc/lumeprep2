1c1e12700182204694249a9f9ca957314f362a3c
Keyur Gajjar <kga@odoo.com>
2021-07-22 18:45:49 -0700

    [MOD] made the metrc location on odoo location field many2many. - Because on odoo location can have metrc locations from multiple facilities. - Improvements on metrc item category views.

--- metrc_stock/models/stock_location.py
+++ metrc_stock/models/stock_location.py
@@ -7,5 +7,11 @@ from odoo import api, fields, models, _
 class StockLocation(models.Model):
     _inherit = 'stock.location'
 
-    metrc_location_id = fields.Many2one(comodel_name='metrc.location',
-                                        string='Metrc Location')
+    metrc_location_ids = fields.Many2many(comodel_name='metrc.location',
+                                        string='Metrc Location(s)')
+    
+    def get_metrc_location(self, facility_license_id):
+        if not self.metrc_location_ids:
+            return False
+        metrc_location = self.metrc_location_ids.filtered(lambda l: l.facility_license_id == facility_license_id)
+        return metrc_location or False
